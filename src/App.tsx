699dc6654ebbb9a4ca7d33f51ebc30df28c285f8
day-xue <739919115@qq.com>
2023-03-27 23:26:27 +0800

    feat: 模拟接口测试

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,51 @@
-import React, { FC } from "react"
+import axios from "axios"
+import { debounce } from "lodash-es"
+import React, { FC, startTransition, useEffect, useState } from "react"
+import { Remarkable } from "remarkable"
 
+const md = new Remarkable()
 const App: FC = props => {
+  const [result, setResult] = useState("")
+  const [input, setInput] = useState("")
+  const [search, setSearch] = useState(false)
+  useEffect(() => {
+    const co = new AbortController()
+    const fetchChat = debounce(() => {
+      axios.post(
+        "https://cleqy3.laf.dev/chat",
+        {
+          question: input,
+        },
+        {
+          onDownloadProgress: e => {
+            const xhr = e.event.target
+            const { responseText } = xhr as { responseText: string }
+            const result = responseText.replace(/["]/g, "")
+
+            startTransition(() => {
+              setResult(result)
+            })
+          },
+          signal: co.signal,
+        }
+      )
+    }, 1000)
+    if (search) fetchChat()
+    return () => {
+      co.abort()
+    }
+  }, [search])
   return (
     <div>
-      <h2>App</h2>
+      <input
+        type="text"
+        onBlur={e => {
+          setInput(e.target.value)
+          setSearch(true)
+        }}
+      />
+      {/* <div dangerouslySetInnerHTML={{ __html: md.render(result) }} /> */}
+      <div dangerouslySetInnerHTML={{ __html: result }}></div>
     </div>
   )
 }
